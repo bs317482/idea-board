cafcca56314d364ea22283fdd326dca128f481de
bs317482 <bernardbnard@gmail.com>
2018-04-24 14:55:33 -0400

    dd

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import LogInPage from './LogInPage'
+
+class HomePage extends Component {
+    render(){
+        return(
+            <div>HomePage
+                <LogInPage/>
+            </div>
+            
+        )
+    }
+}
+
+
+export default HomePage 
---
+++ client/src/components/IdeaPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class IdeaPage extends Component {
+    render(){
+        return(
+            <div>IdeaPage</div>
+        )
+    }
+}
+
+
+export default IdeaPage 
